801b63450288af2e996ddb81b269e953a245d50d
t.frankbutler <t.frankbutler@gmail.com>
2018-08-27 13:04:02 -0400

    Finished Assignment

--- HW/Netbeans Projects/Butler_Lab2_Employee Class/src/butler_lab2_employee/pkgclass/Butler_Lab2_EmployeeClass.java
+++ HW/Netbeans Projects/Butler_Lab2_Employee Class/src/butler_lab2_employee/pkgclass/Butler_Lab2_EmployeeClass.java
@@ -7,6 +7,11 @@ public class Butler_Lab2_EmployeeClass {
         Employee e2 = new Employee("Mark Jones", 39119, "IT", "Programmer");
         Employee e3 = new Employee("Joy Rogers", 81774, "Manufacturing", "Engineer");
         
+        System.out.println("Name          ID Number      Department       Position");
+        System.out.println("---------------------------------------------------------");
+        System.out.println(e1.getName() + "    " + e1.getIdNumber() + "        " + e1.getDepartment() + "    " + e1.getPosition());
+        System.out.println(e2.getName() + "      " + e2.getIdNumber() + "        " + e2.getDepartment() + "              " + e2.getPosition());
+        System.out.println(e3.getName() + "      " + e3.getIdNumber() + "        " + e3.getDepartment() + "    " + e3.getPosition());
         
     }
     
