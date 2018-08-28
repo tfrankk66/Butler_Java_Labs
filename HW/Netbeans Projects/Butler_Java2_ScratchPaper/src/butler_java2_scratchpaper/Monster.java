9408a0208eb6f3162885b1e42f6b301308ed9328
T.frankk(laptop) <t.frankbutler@gmail.com>
2018-08-28 19:43:55 -0400

    Assignment 2 Completion

---
+++ HW/Netbeans Projects/Butler_Java2_ScratchPaper/src/butler_java2_scratchpaper/Monster.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package butler_java2_scratchpaper;
+
+/**
+ *
+ * @author Al
+ */
+public class Monster {
+    
+}
