83ee5d4b382c0a4db1ec742aa93b0da73230b754
t.frankbutler <t.frankbutler@gmail.com>
2018-09-11 12:22:49 -0400

    Java2_Lab3_PasswordVerifier

---
+++ HW/Netbeans Projects/Butler_Java2_Lab3_PasswordVerifier/src/butler_java2_lab3_passwordverifier/Butler_Java2_Lab3_PasswordVerifier.java
@@ -0,0 +1,41 @@
+package butler_java2_lab3_passwordverifier;
+import java.util.Scanner;
+public class Butler_Java2_Lab3_PasswordVerifier {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean hasUpperCase = false;
+        boolean hasLowerCase = false;
+        boolean hasDigit = false;
+        String pass;
+        
+        System.out.println("Hello and Welcome to the Password Verifier Program!");
+        System.out.print("To start, Please enter a password that contains alteast one digit, uppercase letter, lowercase letter and is atleast 6 characters long: ");
+        pass = input.next();
+        
+        if(pass.length() < 6){
+            System.out.print("Password was invalid!");
+        }
+        else{
+            for(int i = 0; i < pass.length(); i++){
+                if(Character.isUpperCase(pass.charAt(i))){
+                    hasUpperCase = true;
+                }
+                else if(Character.isLowerCase(pass.charAt(i))){
+                    hasLowerCase = true;
+                }
+                else if(Character.isDigit(pass.charAt(i))){
+                    hasDigit = true;
+                }
+            }
+
+            if(hasUpperCase && hasLowerCase && hasDigit){
+                System.out.print("Password was valid!");
+            }
+            else{
+                System.out.print("Password was invalid!");
+            }
+        }
+    }
+    
+}
