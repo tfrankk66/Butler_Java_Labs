2fcc63a58c2b6600b75c41d2b7d9a2f545f0b7f2
T.frankk(laptop) <t.frankbutler@gmail.com>
2018-09-23 21:43:04 -0400

    Changes to Scratch paper (9/23/18)

---
+++ HW/Netbeans Projects/Butler_Java2__ScratchPaper/src/butler_java2_scratchpaper/AmericanExpress.java
@@ -0,0 +1,12 @@
+package butler_java2_scratchpaper;
+
+public class AmericanExpress extends Account{
+    private double balance;
+    private double intRate;
+    private int points;
+    
+    public AmericanExpress(){
+        
+    }
+    
+}
