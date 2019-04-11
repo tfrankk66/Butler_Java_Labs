c6dab9d8de58826b146f1be7be79c31aa13ac943
t.frankbutler <t.frankbutler@gmail.com>
2019-04-11 11:20:08 -0400

    Final Project Move

---
+++ GITHW/NETBEANS EE/Butler_Java3_FinalProject/dukes-bookstore/dukes-bookstore/build/generated-sources/ap-source-output/javaeetutorial/dukesbookstore/entity/Book_.java
@@ -0,0 +1,21 @@
+package javaeetutorial.dukesbookstore.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-01-05T16:13:22")
+@StaticMetamodel(Book.class)
+public class Book_ { 
+
+    public static volatile SingularAttribute<Book, String> firstname;
+    public static volatile SingularAttribute<Book, String> surname;
+    public static volatile SingularAttribute<Book, Double> price;
+    public static volatile SingularAttribute<Book, Integer> calendarYear;
+    public static volatile SingularAttribute<Book, String> description;
+    public static volatile SingularAttribute<Book, Boolean> onsale;
+    public static volatile SingularAttribute<Book, String> title;
+    public static volatile SingularAttribute<Book, Integer> inventory;
+    public static volatile SingularAttribute<Book, String> bookId;
+
+}
